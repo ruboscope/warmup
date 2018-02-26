3706240e3eb29a3a0d116bdddfeb7eb3cd4b9bc9
Andrey <ruboscope@gmail.com>
2018-02-26 20:52:37 +0300

    -warmup: fix added more elegant return

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 module.exports = function warmup(temperature) {
   // your implementation here
-  return (9 * temperature) / 5 + 32;
+  //my solution - everything on rules
+  //return (9 * temperature) / 5 + 32;
+  
+  //more elegant solution by epam
+  return temperature * 1.8 + 32;
 };
